bf7bd3b833218b1f769a54bbf68c5ff76e9b7bcc
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-30 02:40:54 +0700

    десятичное число в двоичное-2

---
+++ Задача3-2/Program.cs
@@ -0,0 +1,34 @@
+﻿int number = ReadInt("Введите десятичное число: ");
+int baseNumber = 2;
+int tempNumber = number;
+int count = 0;
+while(tempNumber > 0)
+{
+    count++;
+    tempNumber /= baseNumber;
+}
+int[] binary = new int[count];
+
+for(int i = binary.Length - 1; i >= 0; i--)
+{
+    binary[i] = number % baseNumber;
+    number /= baseNumber;
+}
+
+WriteArray(binary);
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
