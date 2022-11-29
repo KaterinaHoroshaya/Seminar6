33e0719e96eb975539fcf34ec9b90f0e40169fc3
Katerina <m-a-c-u-k-a@mail.ru>
2022-11-30 02:41:35 +0700

    Фибоначчи

---
+++ Задача4-2/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = ReadInt("Введите количество членов ряда Фибоначчи: ");
+int[] fibonacci = new int[count];
+
+if(count > 0)
+    fibonacci[0] = 0;
+if(count > 1)
+    fibonacci[1] = 1;
+
+for (int i = 2; i < count; i++)
+{
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+WriteArray(fibonacci);
+
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
